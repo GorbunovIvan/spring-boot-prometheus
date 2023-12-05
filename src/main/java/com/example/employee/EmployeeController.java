b95b9accac51fb8d6088cb44269d8e8577577958
GorbunovIvan <gorbunovvivan@gmail.com>
2023-12-05 14:25:42 +0200

    some metrics are added to code

--- src/main/java/com/example/employee/EmployeeController.java
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.example.employee;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -14,15 +18,41 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    private MeterRegistry meterRegistry;
+
+    private static final AtomicInteger REQUESTS_TOTAL = new AtomicInteger();
+    private Timer requestDurationTimer;
+
+    @Autowired
+    public void setMeterRegistry(MeterRegistry meterRegistry) {
+
+        this.meterRegistry = meterRegistry;
+
+        // Gauge metric
+        this.meterRegistry.gauge("employee_controller_requests_total", REQUESTS_TOTAL);
+
+        // Histogram metric
+        this.requestDurationTimer = Timer.builder("employee_controller_request_duration_seconds")
+                .description("Request duration in seconds for EmployeeController")
+                .register(this.meterRegistry);
+    }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getAll() {
+        REQUESTS_TOTAL.incrementAndGet();
         return employeeService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getById(@PathVariable long id) {
-        var employee = employeeService.getById(id);
+        Employee employee;
+        Timer.Sample timerSample = Timer.start();
+        try {
+            employee = employeeService.getById(id);
+        } finally {
+            timerSample.stop(requestDurationTimer);
+        }
         if (employee == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(employee, HttpStatus.OK);
--- src/main/java/com/example/employee/EmployeeService.java
+++ src/main/java/com/example/employee/EmployeeService.java
@@ -1,15 +1,30 @@
 package com.example.employee;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Metrics;
 import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class EmployeeService {
 
     private final List<Employee> employees;
+
+    private final static AtomicInteger EMPLOYEES_REQUESTED = new AtomicInteger();
+
+    // Counter metric
+    private static final Counter REQUESTS_TOTAL = Metrics.counter("employee_service_requests_total");
+
+    // Gauge metric
+    private static final Gauge EMPLOYEES_REQUESTED_BY_ID = Gauge.builder("employees_requests_by_id", () -> EMPLOYEES_REQUESTED)
+            .description("IDs of employees that were requested")
+            .tags("tag1", "value1", "tag2", "value2")
+            .register(Metrics.globalRegistry);
 
     public EmployeeService() {
 
@@ -22,10 +37,12 @@ public class EmployeeService {
     }
 
     public List<Employee> getAll() {
+        REQUESTS_TOTAL.increment();
         return new ArrayList<>(employees);
     }
 
     public Employee getById(Long id) {
+        EMPLOYEES_REQUESTED.incrementAndGet();
         return employees.stream()
                 .filter(e -> e.getId().equals(id))
                 .findAny()
